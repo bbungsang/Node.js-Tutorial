b014169487ca465144f3cfd73bddde0f623e1c30
bbungsang <821heejin@naver.com>
2017-12-13 08:57:23 +0900

    Split the Query String

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
 
-http.createServer(function (req, res) {
+http.createServer(function (req, res) { // create a server object
+    // HTTP 서버의 응답을 HTML로 표시하려면 HTTP 헤더를 포함해야 한다.
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("The date and time are currently: " + dt.myDateTime());
-    res.end();
-}).listen(8080);
+    res.end(); // end the response
+}).listen(8080); // the server object listens on port 8080
